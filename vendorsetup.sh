0ddd191cbe2e060787c6521349dc092049bee19b
Jamie Falconer <mrfluffyoven@gmail.com>
2024-07-18 09:44:33 +0100

    Test: change fox flags

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -59,9 +59,9 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
     export FOX_USE_BASH_SHELL=1
     export FOX_ASH_IS_BASH=1
     export FOX_USE_GREP_BINARY=1
-    export FOX_USE_XZ_UTILS=1
+    export FOX_USE_XZ_UTILS=0
     export FOX_USE_NANO_EDITOR=1
-    export OF_ENABLE_LPTOOLS=1
+    export OF_ENABLE_LPTOOLS=0
     export FOX_DELETE_AROMAFM=1
   
 # Display Settings
@@ -72,14 +72,14 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
     export OF_SCREEN_H=2400
 
 # A/B-Related
-    export OF_AB_DEVICE_WITH_RECOVERY_PARTITION=1
+    export OF_AB_DEVICE_WITH_RECOVERY_PARTITION=0
     export FOX_VIRTUAL_AB_DEVICE=0
     export OF_VANILLA_BUILD=0
   
 # Other Patches
-    export OF_NO_RELOAD_AFTER_DECRYPTION=1
+    export OF_NO_RELOAD_AFTER_DECRYPTION=0
     export OF_FBE_METADATA_MOUNT_IGNORE=1
-    export OF_PATCH_AVB20=1
+    export OF_PATCH_AVB20=0
     export OF_NO_SPLASH_CHANGE=1
     export OF_FIX_DECRYPTION_ON_DATA_MEDIA=1
     export FOX_BUGGED_AOSP_ARB_WORKAROUND="1616300800"; # Sun 21 Mar 04:26:40 GMT 2021
